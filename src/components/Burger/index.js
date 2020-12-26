ebf1f82f9f2b3b6560bb110d187c64da5362289f
Wesley <wesaboo@gmail.com>
2020-12-25 17:53:44 -0800

    created state, adds validation for if object contains ingredients, mapped out ingredients

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,11 +3,24 @@ import style from "./index.css";
 import BurgerIngredient from "./BurgerIngredient";
 
 export default function burger(props) {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={style.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -3,10 +3,23 @@ import Auxilary from "../../hoc/Auxilary";
 import Burger from "../../components/Burger";
 
 export default class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
+
   render() {
     return (
       <Auxilary>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxilary>
     );
