76e271fa873cf3dbbf5ceb79b0ff40898e2e3bfa
Wesley <wesaboo@gmail.com>
2020-12-28 17:26:18 -0800

    adds order button, logic for button to be disabled

--- src/components/Burger/BuildControls/index.js
+++ src/components/Burger/BuildControls/index.js
@@ -26,6 +26,9 @@ export default function buildControls(props) {
           />
         );
       })}
+      <button className={style.OrderButton} disabled={!props.purchasable}>
+        ORDER NOW
+      </button>
     </div>
   );
 }
